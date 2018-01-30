389ca165431b77d8f98063a12955ecd1ba3118bc
Wes Reinhart <wfr@princeton.edu>
2018-01-30 15:48:17 -0500

    Updated NGA base classes for more specialized data storage

--- src/nga.py
+++ src/nga.py
@@ -10,13 +10,14 @@ from __future__ import print_function
 import numpy as np
 from emd import emd
 
-import sys
+import pickle
 
-sys.path.append('/home/wfr/freud/freud_0.6.3')
-import freud
+import sys
 
 sys.path.append('/home/wfr/crayon/build')
 import crayon
+
+from util_neighborlist import *
 
 class Graph:
     R""" evaluates topology of neighborhood
@@ -51,14 +52,46 @@ class Graph:
         """
         return str(self.gdd.tolist())
 
-class Snapshot:
+class GraphLibrary:
+    def __init__(self):
+        self.library = {}
+        self.index_map = {}
+    def encounter(self,key,entry):
+        sig = key
+        G = entry['graph']
+        C = entry['count']
+        sigs = np.asarray(self.library.keys(),dtype=np.str)
+        match = np.argwhere( sigs == sig ).flatten()
+        # match = [s for s in self.library.keys() if s == sig]
+        # check graphs with matching GDD for true (GDV-EMD) equivalence
+        idx = None
+        for m in match:
+            if self.library[sigs[m]]['graph'] == G:
+                idx = m
+                self.library[sigs[m]]['count'] += C
+                break
+        if idx is None:
+            self.library[sig] = {'graph': G, 'count': C}
+            idx = len(sigs)
+            self.index_map[idx] = sig
+        return idx
+    def collect(self,others):
+        if type(others) != list:
+            others = list([others])
+        # iterate over supplied library instances
+        for other in others:
+            for key in other:
+                self.encounter(key,other[key])
+
+class Snapshot(GraphLibrary):
     R""" identifies neighborhoods from simulation snapshot
 
     Args:
         xyz (array-like): N x 3 array of particle positions
         L (array-like): box dimensions in x, y, z
+        pbc (str): dimensions with periodic boundaries (defaults to 'xyz')
     """
-    def __init__(self,xyz=None,L=None):
+    def __init__(self,xyz=None,L=None,pbc='xyz'):
         # check that variable types and sizes match expected
         if xyz is not None:
             try:
@@ -69,6 +102,8 @@ class Snapshot:
                 raise Exception('Error: xyz must have 3 columns')
         else:
             raise Exception('Error: must provide xyz (array of particle coordinates)')
+        self.xyz = xyz
+        self.N = len(xyz)
         if L is not None:
             try:
                 L = np.asarray(L)
@@ -78,6 +113,8 @@ class Snapshot:
                 raise Exception('Error: L must have 3 elements')
         else:
             raise Exception('Error: must provide L (box dimensions)')
+        self.L = L
+        self.pbc = pbc
         # nlAllowed = ['Delaunay','adaptiveCNA']
         # if nlType not in nlAllowed:
         #     raise Exception('Error: nlType must be one of the following: %s'%', '.join(nlAllowed))
@@ -85,66 +122,33 @@ class Snapshot:
         #     if p.lower() not in 'xyz':
         #         raise Exception('Error: periodic boundary conditions must be combination of x, y, and z')
         # assign snapshot data
-        self.xyz = xyz
-        self.L = L
-        self.N = len(xyz)
-        self.NL = None
-        self.neighbors = None
-    def buildNeighborList(self,rmax=40.,nnbr=16,strict_rcut=True):
-        self.box = freud.box.Box(Lx=self.L[0],Ly=self.L[1],Lz=self.L[2],is2D=False)
-        self.NL  = freud.locality.NearestNeighbors(rmax,nnbr,strict_cut=strict_rcut)
-        self.NL.compute(self.box,self.xyz,self.xyz)
-    def getNeighbors(self,nnbr=6,geoFactor=1.2071):
-        if self.NL is None:
-            self.buildNeighborList()
-        Rsq = self.NL.getRsqList()
-        Rsq[Rsq < 0] = np.nan
-        self.Rsq = Rsq
-        R6 = np.nanmean(Rsq[:,:nnbr],axis=1)
-        self.Rcut = geoFactor**2. * R6
-        NL = self.NL.getNeighborList()
-        self.neighbors = []
-        for i in range(self.N):
-            self.neighbors.append(NL[i,self.Rsq[i,:]<self.Rcut[i]])
+        self.NL = AdaptiveCNA(self)
+        # self.NL = Voronoi(self)
+        self.NN = None
+        self.library = {}
+        self.index_map = {}
+        self.graph_index = np.zeros(self.N) - 1
     def getNeighborhoods(self):
-        if self.neighbors is None:
-            self.getNeighbors()
+        if self.NL.neighbors is None:
+            self.NL.build()
         self.NN = []
         for i in range(self.N):
-            n = len(self.neighbors[i])
-            A = np.zeros((n,n),np.int8)
-            idx = self.neighbors[i]
-            for j in range(n):
-                for k in range(j,n):
-                    j_in_k = idx[j] in self.neighbors[idx[k]]
-                    k_in_j = idx[k] in self.neighbors[idx[j]]
-                    edge = int( j_in_k or k_in_j or j == k )
-                    A[j,k] = edge
-                    A[k,j] = edge
-            self.NN.append(A)
+            self.NN.append(neighborsToAdjacency(i,self.NL.neighbors))
         return self.NN
-
-class Ensemble:
-    def __init__(self):
-        self.structures = {}
-    def examine(self,S):
-        NN = S.getNeighborhoods()
+    def buildLibrary(self):
+        if self.NN is None:
+            self.getNeighborhoods()
+        self.graphs = {}
+        self.counts = {}
+        self.graph_index = np.zeros(self.N)
         # build graphs from neighborhoods
-        for i in range(S.N):
-            G = Graph(NN[i])
-            self.encounter(G)
-    def encounter(self,G):
-        sig = str(G)
-        new = True
-        match = [s for s in self.structures.keys() if s == sig]
-        # check graphs with matching GDD for true (GDV-EMD) equivalence
-        for m in match:
-            if self.structures[m] == G:
-                new = False
-                break
-        if new:
-            self.structures[sig] = G
-    def coalesce(self,others):
-        for other in others:
-            for key in other:
-                self.encounter(other[key])
+        for i in range(self.N):
+            G = Graph(self.NN[i])
+            key = str(G)
+            entry = {'graph': G, 'count': 1}
+            self.graph_index[i] = self.encounter(key,entry)
+
+class Ensemble(GraphLibrary):
+    def __init__(self):
+        self.library = {}
+        self.index_map = {}
