8fb14346850304286da4a7add370cd1d32d3d823
Wes Reinhart <wfr@princeton.edu>
2018-04-04 12:35:14 -0400

    Minor changes to structure of bondorder code.

--- src/py/bondorder.py
+++ src/py/bondorder.py
@@ -35,7 +35,7 @@ def ql(rij,l):
     q *= 4*np.pi/(2*l+1)
     return np.sqrt(q)
 
-def computeQRange(snap):
+def computeQ(snap,averaged=True):
     q = np.zeros((snap.N,11))
     for i in range(snap.N):
         nn = snap.neighbors[i]
@@ -43,9 +43,15 @@ def computeQRange(snap):
         rij = snap.wrap(snap.xyz[nn,:] - snap.xyz[i,:])
         q[i,:] = [ql(rij,2),ql(rij,3),ql(rij,4),ql(rij,5),ql(rij,6),ql(rij,7), \
                   ql(rij,8),ql(rij,9),ql(rij,10),ql(rij,11),ql(rij,12)]
+    if not averaged:
+        return q
     q_avg = np.zeros((snap.N,11))
     for i in range(snap.N):
         q_avg[i,:] = np.mean(q[snap.neighbors[i],:],axis=0)
+    return q_avg
+
+def computeQRange(snap):
+    q_avg = computeQ(snap)
     q_range = np.max(q_avg,axis=1) - np.min(q_avg,axis=1)
     return q_range
 
