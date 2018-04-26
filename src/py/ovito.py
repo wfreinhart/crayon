d8512c0f326794864b1d894b2a27d9dee48f1e49
Wes Reinhart <wfr@princeton.edu>
2018-04-26 16:51:21 -0400

    Added support for GSD trajectories.

--- src/py/ovito.py
+++ src/py/ovito.py
@@ -10,8 +10,12 @@ import numpy as np
 
 def modify(frame, input, output):
 	source = input.attributes['SourceFile']
+	frame = input.attributes['SourceFrame']
 	print(source)
-	cmap = np.loadtxt(source + '.cmap')
+	try:
+		cmap = np.loadtxt(source + '.cmap')
+	except:
+		cmap = np.loadtxt(source + '-%d.cmap'%frame)
 	color_property = output.create_particle_property(ovito.data.ParticleProperty.Type.Color)
 	N = len(color_property.marray)
 	for i in range(N):
