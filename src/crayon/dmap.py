0d20cea81104c160106d72d65f8634c603deaa6b
wfr <wfr@princeton.edu>
2020-02-17 19:40:12 -0500

    Added pure python implementation of dmap.

--- src/crayon/dmap.py
+++ src/crayon/dmap.py
@@ -1,6 +1,6 @@
 #
 # dmap.py
-# wraps PyDMap diffusion map calculations for easy scripting
+# pure python implementation of basic diffusion map calculation
 #
 # Copyright (c) 2018 Wesley Reinhart.
 # This file is part of the crayon project, released under the Modified BSD License.
@@ -13,22 +13,41 @@ import numpy as np
 from crayon import parallel
 from crayon import util
 
-try:
-    import PyDMap
-except:
-    raise RuntimeError('dmap submodule requires PyDMap python module')
-
 class DMap:
     R""" container for computing diffusion maps from Ensembles of Snapshots """
-    def __init__(self):
-        self._cpp = PyDMap.DMap()
+    def __init__(self, num_evec=4):
         self.alpha = 1.0
-        self.num_evec = 4
+        self.num_evec = num_evec
         self.epsilon = None
         self.evals = None
         self.evecs = None
         self.evecs_ny = None
         self.coords = None
+    def compute(self,dists,epsilon):
+        R"""Compute diffusion map embedding from square distance matrix."""
+        #
+        self.epsilon = epsilon
+        self.dists = dists
+        #
+        inv_neg_2eps = -1./(2.*self.epsilon)
+        kernel = np.exp(dists**2 * inv_neg_2eps)
+        rsum = np.sum(kernel, axis=1)
+        kernel = np.matmul( np.diag(1./rsum), kernel )
+        #
+        evals, evecs = np.linalg.eig(kernel)
+        evals = np.real(evals)
+        evecs = np.real(evecs)
+        idx = np.argsort(evals)[::-1]
+        #
+        self.evals = evals[idx][1:(1+self.num_evec)]
+        self.evecs = evecs[:,idx][:,1:(1+self.num_evec)]
+    def embed(self,dists):
+        R"""Embed new samples using Nystrom approximation."""
+        d = np.exp(-dists**2/(2.*self.epsilon))
+        inv_rsum = 1./np.sum(d,axis=1)
+        d *= inv_rsum.reshape(-1,1)
+        loc = np.matmul( d, self.eigenvec ) / self.eigenval
+        return loc
     def build(self,dists,landmarks=None,
               valid_cols=None,
               valid_rows=None):
@@ -54,20 +73,27 @@ class DMap:
             D = D[:,valid_cols]
             L = alpha_dists[valid_rows,:]
             L = L[:,valid_cols]
-        # compute landmark manifold
-        self._cpp.set_dists(D)
-        self._cpp.set_num_evec(self.num_evec)
+        # default to median of distances
         if self.epsilon is None:
             self.epsilon = np.median(alpha_dists)
-        self._cpp.set_epsilon(self.epsilon)
-        self._cpp.compute()
-        self.evals = np.asarray(self._cpp.get_eval())
-        self.evecs = np.asarray(self._cpp.get_evec())
+        # compute landmark manifold
+        self.compute(D, self.epsilon)
         # embed remaining graphs using landmarks
-        if landmarks is None:
-            self.evecs_ny = None
-        else:
-            self.evecs_ny = np.asarray(PyDMap.nystrom(self._cpp,L))
+        self.evecs_ny = self.embed(L)
+        # # compute landmark manifold (old way!)
+        # self._cpp.set_dists(D)
+        # self._cpp.set_num_evec(self.num_evec)
+        # if self.epsilon is None:
+        #     self.epsilon = np.median(alpha_dists)
+        # self._cpp.set_epsilon(self.epsilon)
+        # self._cpp.compute()
+        # self.evals = np.asarray(self._cpp.get_eval())
+        # self.evecs = np.asarray(self._cpp.get_evec())
+        # # embed remaining graphs using landmarks (old way!)
+        # if landmarks is None:
+        #     self.evecs_ny = None
+        # else:
+        #     self.evecs_ny = np.asarray(PyDMap.nystrom(self._cpp,L))
         # backfill for invalid graphs
         evecs = np.zeros((dists.shape[1],self.num_evec))*np.nan
         evecs[valid_cols,:] = np.array(self.evecs)
